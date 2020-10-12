67c29e119310fa93e8f9c477a8c1d406bf1b8f45
Narendra Singh <narendra.cs.biet@gmail.com>
2020-10-12 19:38:27 +0530

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("Narendra Singh");
         System.out.println("Hello, World!");
-	System.out.println("Build on git repo push to github");
     }
 }
